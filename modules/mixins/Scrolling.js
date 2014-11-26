e6643b32970fc5b5eef0ad8bf40397ed1abf7005
Ryan Wang <happycamper.sw@gmail.com>
2014-11-26 18:57:54 +0100

    Record scroll position in dispatch

--- modules/mixins/Scrolling.js
+++ modules/mixins/Scrolling.js
@@ -1,6 +1,30 @@
 var invariant = require('react/lib/invariant');
 var canUseDOM = require('react/lib/ExecutionEnvironment').canUseDOM;
 var getWindowScrollPosition = require('../utils/getWindowScrollPosition');
+var Path = require('../utils/Path');
+
+function shouldUpdateScroll(state, prevState) {
+  if (!prevState) {
+    return true;
+  }
+
+  var path = state.path;
+  var routes = state.routes;
+  var prevPath = prevState.path;
+  var prevRoutes = prevState.routes;
+
+  if (Path.withoutQuery(path) === Path.withoutQuery(prevPath)) {
+    return false;
+  }
+
+  var sharedAncestorRoutes = routes.filter(function (route) {
+    return prevRoutes.indexOf(route) !== -1;
+  });
+
+  return !sharedAncestorRoutes.some(function (route) {
+    return route.ignoreScrollBehavior;
+  });
+}
 
 /**
  * Provides the router with the ability to manage window scroll position
@@ -8,44 +32,53 @@ var getWindowScrollPosition = require('../utils/getWindowScrollPosition');
  */
 var Scrolling = {
 
+  statics: {
+    /**
+     * Records curent scroll position as the last known position for the given URL path.
+     */
+    recordScrollPosition: function (path) {
+      if (!this.scrollHistory)
+        this.scrollHistory = {};
+
+      this.scrollHistory[path] = getWindowScrollPosition();
+    },
+
+    /**
+     * Returns the last known scroll position for the given URL path.
+     */
+    getScrollPosition: function (path) {
+      if (!this.scrollHistory)
+        this.scrollHistory = {};
+
+      return this.scrollHistory[path] || null;
+    }
+  },
+
   componentWillMount: function () {
     invariant(
       this.getScrollBehavior() == null || canUseDOM,
       'Cannot use scroll behavior without a DOM'
     );
-
-    this._scrollHistory = {};
   },
 
   componentDidMount: function () {
     this._updateScroll();
   },
 
-  componentWillUpdate: function () {
-    this._scrollHistory[this.state.path] = getWindowScrollPosition();
+  componentDidUpdate: function (prevProps, prevState) {
+    this._updateScroll(prevState);
   },
 
-  componentDidUpdate: function () {
-    this._updateScroll();
-  },
+  _updateScroll: function (prevState) {
+    if (!shouldUpdateScroll(this.state, prevState)) {
+      return;
+    }
 
-  componentWillUnmount: function () {
-    delete this._scrollHistory;
-  },
-
-  /**
-   * Returns the last known scroll position for the given URL path.
-   */
-  getScrollPosition: function (path) {
-    return this._scrollHistory[path] || null;
-  },
-
-  _updateScroll: function () {
     var scrollBehavior = this.getScrollBehavior();
 
     if (scrollBehavior)
       scrollBehavior.updateScrollPosition(
-        this.getScrollPosition(this.state.path),
+        this.constructor.getScrollPosition(this.state.path),
         this.state.action
       );
   }
