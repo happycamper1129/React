83c3f7728a9339a88e67e35dbf4658e72d71ea51
Ryan Wang <happycamper.sw@gmail.com>
2015-10-19 19:49:49 +0200

    Added a UMD example

--- examples/auth-with-shared-root/components/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-var About = React.createClass({
-  render() {
-    return <h1>About</h1>
-  }
-})
-
-export default About
--- examples/auth-with-shared-root/components/PageOne.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-var PageOne = React.createClass({
-  render() {
-    return <h2>Page One!</h2>
-  }
-})
-
-export default PageOne
