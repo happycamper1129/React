2ab2f70da637db57b8aff007855958c89bfd061f
Ryan Wang <happycamper.sw@gmail.com>
2016-09-13 07:45:43 -0700

    tweak release script for manual prerelease

---
+++ modules/StaticRouter.js
@@ -0,0 +1,96 @@
+import { stringify, parse as parseQuery } from 'query-string'
+import React, { PropTypes } from 'react'
+import MatchProvider from './MatchProvider'
+import { createRouterLocation, createRouterPath } from './LocationUtils'
+import {
+  action as actionType,
+  location as locationType,
+  router as routerType
+} from './PropTypes'
+
+const defaultStringifyQuery = (query) => (
+  stringify(query).replace(/%20/g, '+')
+)
+
+class StaticRouter extends React.Component {
+
+  static propTypes = {
+    action: actionType.isRequired,
+    blockTransitions: PropTypes.func,
+    children: PropTypes.oneOfType([ PropTypes.node, PropTypes.func ]),
+    createHref: PropTypes.func.isRequired,
+    location: PropTypes.oneOfType([ PropTypes.object, PropTypes.string ]).isRequired,
+    onPush: PropTypes.func.isRequired,
+    onReplace: PropTypes.func.isRequired,
+    stringifyQuery: PropTypes.func.isRequired,
+    // TODO: parseQueryString
+    parseQuery: PropTypes.func.isRequired
+  }
+
+  static defaultProps = {
+    createHref: path => path,
+    stringifyQuery: defaultStringifyQuery,
+    parseQuery
+  }
+
+  static childContextTypes = {
+    router: routerType.isRequired,
+    location: locationType.isRequired
+  }
+
+  createLocationForContext(loc) {
+    const { parseQuery, stringifyQuery } = this.props
+    return createRouterLocation(loc, parseQuery, stringifyQuery)
+  }
+
+  getChildContext() {
+    const createHref = (to) => {
+      const path = createRouterPath(to, this.props.stringifyQuery)
+      return this.props.createHref(path)
+    }
+
+    const location = this.getLocation()
+
+    return {
+      location,
+      router: {
+        createHref,
+        transitionTo: (loc) => {
+          this.props.onPush(this.createLocationForContext(loc))
+        },
+        replaceWith: (loc) => {
+          this.props.onReplace(this.createLocationForContext(loc))
+        },
+        blockTransitions: (getPromptMessage) => {
+          this.props.blockTransitions(getPromptMessage)
+        }
+      }
+    }
+  }
+
+  getLocation() {
+    // TODO: maybe memoize this on willReceiveProps to get extreme w/ perf
+    const { location, parseQuery, stringifyQuery } = this.props
+    return createRouterLocation(location, parseQuery, stringifyQuery)
+  }
+
+  render() {
+    const { children } = this.props
+    const location = this.getLocation()
+
+    return (
+      <MatchProvider>
+        {typeof children === 'function' ? (
+          children({ location, router: this.getChildContext().router })
+        ) : React.Children.count(children) > 1 ? (
+          // TODO: get rid of all DOM stuff
+          <div>{children}</div>
+        ) : (
+          children
+        )}
+      </MatchProvider>
+    )
+  }
+}
+
+export default StaticRouter
---
+++ modules/__tests__/StaticRouter-test.js
@@ -0,0 +1,192 @@
+import expect from 'expect'
+import React from 'react'
+import StaticRouter from '../StaticRouter'
+import Link from '../Link'
+import Redirect from '../Redirect'
+import { renderToString } from 'react-dom/server'
+
+//console.error = () => {}
+
+// is there a bug in expect? I thought it handled nested objects
+const expectDeepEquality = (actual, expected) => {
+  Object.keys(actual).forEach(key => {
+    if (typeof actual[key] === 'object' && actual[key] != null) {
+      expectDeepEquality(actual[key], expected[key])
+    } else {
+      expect(actual[key]).toEqual(expected[key])
+    }
+  })
+}
+
+describe('StaticRouter', () => {
+
+  const requiredProps = {
+    location: '/',
+    action: 'POP',
+    createHref: () => {},
+    blockTransitions: () => {}, // we sure we want this required? servers don't need it.
+    onPush: () => {},
+    onReplace: () => {}
+  }
+
+  describe('rendering', () => {
+    it('renders static children', () => {
+      expect(renderToString(
+        <StaticRouter {...requiredProps}>
+          <div>test</div>
+        </StaticRouter>
+      )).toContain('test')
+    })
+
+    it('passes the location to function children', () => {
+      let actualLocation
+      renderToString(
+        <StaticRouter {...requiredProps} location="/lol">
+          {({ location }) => <div>{(actualLocation = location, null)}</div>}
+        </StaticRouter>
+      )
+      expectDeepEquality(actualLocation, {
+        action: 'POP',
+        hash: '',
+        key: null,
+        pathname: '/lol',
+        search: '',
+        query: null,
+        state: null
+      })
+    })
+  })
+
+  describe('location prop', () => {
+    it('parses string `location` into a real location', () => {
+      let actualLocation
+      renderToString(
+        <StaticRouter
+          {...requiredProps}
+          location="/lol?foo=bar"
+        >
+          {({ location }) => (
+            <div>{(actualLocation = location, null)}</div>
+          )}
+        </StaticRouter>
+      )
+      const expected = {
+        action: 'POP',
+        hash: '',
+        pathname: '/lol',
+        search: '?foo=bar',
+        query: { foo: 'bar' },
+        state: null
+      }
+      expectDeepEquality(actualLocation, expected)
+    })
+
+    describe('location descriptors', () => {
+      const assertParsedDescriptor = (loc, expected) => {
+        let actualLocation
+        renderToString(
+          <StaticRouter {...requiredProps} location={loc}>
+            {({ location }) => <div>{(actualLocation = location, null)}</div>}
+          </StaticRouter>
+        )
+
+        expectDeepEquality(actualLocation, expected)
+      }
+
+      it('adds default properties', () => {
+        assertParsedDescriptor({}, {
+          pathname: '',
+          query: null,
+          hash: '',
+          state: null,
+          search: ''
+        })
+      })
+
+      it('parses query to add search', () => {
+        assertParsedDescriptor({
+          query: { a: 'b' }
+        }, {
+          pathname: '',
+          query: { a: 'b' },
+          hash: '',
+          state: null,
+          search: '?a=b'
+        })
+      })
+
+      it('stringifies search to add query', () => {
+        assertParsedDescriptor({
+          search: '?a=b'
+        }, {
+          pathname: '',
+          query: { a: 'b' },
+          hash: '',
+          state: null,
+          search: '?a=b'
+        })
+      })
+
+      it('uses search if provided', () => {
+        assertParsedDescriptor({
+          search: '?a=b'
+        }, {
+          pathname: '',
+          query: { a: 'b' },
+          hash: '',
+          state: null,
+          search: '?a=b'
+        })
+      })
+
+      it('uses query if provided', () => {
+        assertParsedDescriptor({
+          query: { a: 'b' }
+        }, {
+          pathname: '',
+          query: { a: 'b' },
+          hash: '',
+          state: null,
+          search: '?a=b'
+        })
+      })
+
+      it('uses pathname if provided', () => {
+        assertParsedDescriptor({
+          pathname: '/somewhere'
+        }, {
+          pathname: '/somewhere',
+          query: null,
+          hash: '',
+          state: null,
+          search: ''
+        })
+      })
+
+      it('uses state if provided', () => {
+        assertParsedDescriptor({
+          state: { status: 301 }
+        }, {
+          pathname: '',
+          query: null,
+          hash: '',
+          state: { status: 301 },
+          search: ''
+        })
+      })
+
+      it('uses hash if provided', () => {
+        assertParsedDescriptor({
+          hash: '#hi'
+        }, {
+          pathname: '',
+          query: null,
+          hash: '#hi',
+          state: null,
+          search: ''
+        })
+      })
+
+    })
+  })
+})
