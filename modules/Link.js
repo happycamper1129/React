3b174a0c900f46159dc13865604f1569565d2744
Ryan Wang <happycamper.sw@gmail.com>
2016-09-14 22:09:59 +0900

    Update README.md (#3833)

--- modules/Link.js
+++ modules/Link.js
@@ -1,163 +1,134 @@
-import React, { PropTypes } from 'react'
-import {
-  location as locationType,
-  router as routerType
-} from './PropTypes'
+import React from 'react'
+import invariant from 'invariant'
+import { routerShape } from './PropTypes'
+import { ContextSubscriber } from './ContextUtils'
 
-class Link extends React.Component {
-  static propTypes = {
-    to: PropTypes.oneOfType([ PropTypes.string, PropTypes.object ]).isRequired,
-    activeStyle: PropTypes.object,
-    activeClassName: PropTypes.string,
-    location: PropTypes.object,
-    activeOnlyWhenExact: PropTypes.bool,
-    isActive: PropTypes.func,
+const { bool, object, string, func, oneOfType } = React.PropTypes
 
-    // props we have to deal with but aren't necessarily
-    // part of the Link API
-    style: PropTypes.object,
-    className: PropTypes.string,
-    target: PropTypes.string,
-    onClick: PropTypes.func
-  }
+function isLeftClickEvent(event) {
+  return event.button === 0
+}
 
-  static defaultProps = {
-    activeOnlyWhenExact: false,
-    className: '',
-    activeClassName: '',
-    style: {},
-    activeStyle: {},
-    isActive: (location, to, props) => {
-      return pathIsActive(
-        to.pathname,
-        location.pathname,
-        props.activeOnlyWhenExact
-      ) && queryIsActive(
-        to.query,
-        location.query
-      )
+function isModifiedEvent(event) {
+  return !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
+}
+
+// TODO: De-duplicate against hasAnyProperties in createTransitionManager.
+function isEmptyObject(object) {
+  for (const p in object)
+    if (Object.prototype.hasOwnProperty.call(object, p))
+      return false
+
+  return true
+}
+
+function resolveToLocation(to, router) {
+  return typeof to === 'function' ? to(router.location) : to
+}
+
+/**
+ * A <Link> is used to create an <a> element that links to a route.
+ * When that route is active, the link gets the value of its
+ * activeClassName prop.
+ *
+ * For example, assuming you have the following route:
+ *
+ *   <Route path="/posts/:postID" component={Post} />
+ *
+ * You could use the following component to link to that route:
+ *
+ *   <Link to={`/posts/${post.id}`} />
+ *
+ * Links may pass along location state and/or query string parameters
+ * in the state/query props, respectively.
+ *
+ *   <Link ... query={{ show: true }} state={{ the: 'state' }} />
+ */
+const Link = React.createClass({
+
+  mixins: [ ContextSubscriber('router') ],
+
+  contextTypes: {
+    router: routerShape
+  },
+
+  propTypes: {
+    to: oneOfType([ string, object, func ]),
+    query: object,
+    hash: string,
+    state: object,
+    activeStyle: object,
+    activeClassName: string,
+    onlyActiveOnIndex: bool.isRequired,
+    onClick: func,
+    target: string
+  },
+
+  getDefaultProps() {
+    return {
+      onlyActiveOnIndex: false,
+      style: {}
     }
-  }
+  },
 
-  static contextTypes = {
-    router: routerType, // TODO: This should be required, lazy testers be damned
-    location: locationType // TODO: This should also be required
-  }
-
-  handleClick = (event) => {
+  handleClick(event) {
     if (this.props.onClick)
       this.props.onClick(event)
 
-    if (
-      !event.defaultPrevented && // onClick prevented default
-      !this.props.target && // let browser handle "target=_blank" etc.
-      !isModifiedEvent(event) &&
-      isLeftClickEvent(event)
-    ) {
-      event.preventDefault()
-      this.context.router.transitionTo(this.props.to)
-    }
-  }
+    if (event.defaultPrevented)
+      return
+
+    const { router } = this.context
+    invariant(
+      router,
+      '<Link>s rendered outside of a router context cannot navigate.'
+    )
+
+    if (isModifiedEvent(event) || !isLeftClickEvent(event))
+      return
+
+    // If target prop is set (e.g. to "_blank"), let browser handle link.
+    /* istanbul ignore if: untestable with Karma */
+    if (this.props.target)
+      return
+
+    event.preventDefault()
+
+    router.push(resolveToLocation(this.props.to, router))
+  },
 
   render() {
+    const { to, activeClassName, activeStyle, onlyActiveOnIndex, ...props } = this.props
+
+    // Ignore if rendered outside the context of router to simplify unit testing.
     const { router } = this.context
-    const {
-      to,
-      style,
-      activeStyle,
-      className,
-      activeClassName,
-      location,
-      isActive: getIsActive,
-      activeOnlyWhenExact, // eslint-disable-line
-      ...rest
-    } = this.props
 
-    const currentLocation = location || this.context.location
+    if (router) {
+      // If user does not specify a `to` prop, return an empty anchor tag.
+      if (to == null) { return <a {...props} /> }
 
-    const isActive = getIsActive(
-      currentLocation,
-      createLocationDescriptor(to),
-      this.props
-    )
+      const toLocation = resolveToLocation(to, router)
+      props.href = router.createHref(toLocation)
 
-    // Maybe we should use <Match> here? Not sure how the custom `isActive`
-    // prop would shake out, also, this check happens a LOT so maybe its good
-    // to optimize here w/ a faster isActive check, so we'd need to bench mark
-    // any attempt at changing to use <Match>
-    return (
-      <a
-        {...rest}
-        href={router ? router.createHref(to) : to}
-        onClick={this.handleClick}
-        style={isActive ? { ...style, ...activeStyle } : style }
-        className={isActive ?
-          [ className, activeClassName ].join(' ').trim() : className
+      if (activeClassName || (activeStyle != null && !isEmptyObject(activeStyle))) {
+        if (router.isActive(toLocation, onlyActiveOnIndex)) {
+          if (activeClassName) {
+            if (props.className) {
+              props.className += ` ${activeClassName}`
+            } else {
+              props.className = activeClassName
+            }
+          }
+
+          if (activeStyle)
+            props.style = { ...props.style, ...activeStyle }
         }
-      />
-    )
-  }
-}
-
-// we should probably use LocationUtils.createLocationDescriptor
-const createLocationDescriptor = (to) =>
-  typeof to === 'object' ? to : { pathname: to }
-
-const pathIsActive = (to, pathname, activeOnlyWhenExact) =>
-  activeOnlyWhenExact ? pathname === to : pathname.indexOf(to) === 0
-
-const queryIsActive = (query, activeQuery) => {
-  if (activeQuery == null)
-    return query == null
-
-  if (query == null)
-    return true
-
-  return deepEqual(query, activeQuery)
-}
-
-const isLeftClickEvent = (event) =>
-  event.button === 0
-
-const isModifiedEvent = (event) =>
-  !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
-
-const deepEqual = (a, b) => {
-  if (a == b)
-    return true
-
-  if (a == null || b == null)
-    return false
-
-  if (Array.isArray(a)) {
-    return (
-      Array.isArray(b) &&
-      a.length === b.length &&
-      a.every((item, index) => deepEqual(item, b[index]))
-    )
-  }
-
-  if (typeof a === 'object') {
-    for (let p in a) {
-      if (!Object.prototype.hasOwnProperty.call(a, p)) {
-        continue
-      }
-
-      if (a[p] === undefined) {
-        if (b[p] !== undefined) {
-          return false
-        }
-      } else if (!Object.prototype.hasOwnProperty.call(b, p)) {
-        return false
-      } else if (!deepEqual(a[p], b[p])) {
-        return false
       }
     }
 
-    return true
+    return <a {...props} onClick={this.handleClick} />
   }
 
-  return String(a) === String(b)
-}
+})
 
 export default Link
