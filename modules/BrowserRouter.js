4d03d73bb85451fbc0a2e4211b8d1bc291adf17f
Ryan Wang <happycamper.sw@gmail.com>
2016-09-17 19:58:45 -0700

    Whitespace tweaks and consistent component style

---
+++ modules/BrowserRouter.js
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react'
+import BrowserHistory from 'react-history/BrowserHistory'
+import StaticRouter from './StaticRouter'
+
+/**
+ * A router that uses the HTML5 history API.
+ */
+const BrowserRouter = ({ basename, keyLength, ...rest }) => (
+  <BrowserHistory basename={basename} keyLength={keyLength}>
+    {({ history, action, location }) => (
+      <StaticRouter
+        action={action}
+        location={location}
+        basename={basename}
+        onPush={history.push}
+        onReplace={history.replace}
+        blockTransitions={history.block}
+        {...rest}
+      />
+    )}
+  </BrowserHistory>
+)
+
+BrowserRouter.propTypes = {
+  basename: PropTypes.string,
+  keyLength: PropTypes.number,
+  children: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.node
+  ])
+}
+
+export default BrowserRouter
