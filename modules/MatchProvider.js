114cc82aa6c27acaee14658cc0d67437940eed49
Ryan Wang <happycamper.sw@gmail.com>
2016-10-03 11:28:53 -0700

    add test for match decoding

--- modules/MatchProvider.js
+++ modules/MatchProvider.js
@@ -68,8 +68,13 @@ class MatchProvider extends React.Component {
     // React's contract is that cDM of descendants is called before cDM of
     // ancestors, so here we can safely check if we found a match
     if (this.subscribers.length) {
+      const hadMatches = this.hasMatches
       this.hasMatches = this.matches.length !== 0
-      this.subscribers.forEach(fn => fn(this.hasMatches))
+      // optimization, don't notify if nothing changed initial will be null, so
+      // we can get initial render correct
+      if (this.hasMatches !== hadMatches) {
+        this.subscribers.forEach(fn => fn(this.hasMatches))
+      }
     }
   }
 
