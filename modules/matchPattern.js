2d5e86f9e76bc157802d00f81a21871707cf1ff4
Ryan Wang <happycamper.sw@gmail.com>
2016-10-06 09:19:05 +0200

    don't incude a trailing slash in the href for pathname='/' if the basename is not empty

--- modules/matchPattern.js
+++ modules/matchPattern.js
@@ -19,7 +19,7 @@ const truncatePathnameToPattern = (pathname, pattern) =>
 
 const parseParams = (pattern, match, keys) =>
   match.slice(1).reduce((params, value, index) => {
-    params[keys[index].name] = decodeURIComponent(value)
+    params[keys[index].name] = value
     return params
   }, {})
 
