68060097d3432715a116c8793f50246e81bf4c6c
Ryan Wang <happycamper.sw@gmail.com>
2016-10-25 01:07:35 +0100

    feat: cancel pending enter/change hooks on location change (#4063)

--- modules/__tests__/Redirect-test.js
+++ modules/__tests__/Redirect-test.js
@@ -1,11 +1,60 @@
+import expect from 'expect'
 import React from 'react'
+import { render, unmountComponentAtNode } from 'react-dom'
+import createHistory from '../createMemoryHistory'
 import Redirect from '../Redirect'
-import { renderToString } from 'react-dom/server'
+import Router from '../Router'
+import Route from '../Route'
 
-describe('Redirect', () => {
-  it('does not freak out when rendered out of context', () => {
-    const div = document.createElement('div')
-    renderToString(<Redirect to="/nowhere"/>, div)
+describe('A <Redirect>', function () {
+
+  let node
+  beforeEach(function () {
+    node = document.createElement('div')
   })
-})
 
+  afterEach(function () {
+    unmountComponentAtNode(node)
+  })
+
+  it('works', function (done) {
+    render((
+      <Router history={createHistory('/notes/5')}>
+        <Route path="/messages/:id" />
+        <Redirect from="/notes/:id" to="/messages/:id" />
+      </Router>
+    ), node, function () {
+      expect(this.state.location.pathname).toEqual('/messages/5')
+      done()
+    })
+  })
+
+  it('works with relative paths', function (done) {
+    render((
+      <Router history={createHistory('/nested/route1')}>
+        <Route path="nested">
+          <Route path="route2" />
+          <Redirect from="route1" to="route2" />
+        </Route>
+      </Router>
+    ), node, function () {
+      expect(this.state.location.pathname).toEqual('/nested/route2')
+      done()
+    })
+  })
+
+  it('works with relative paths with param', function (done) {
+    render((
+      <Router history={createHistory('/nested/1/route1')}>
+        <Route path="nested/:id">
+          <Route path="route2" />
+          <Redirect from="route1" to="route2" />
+        </Route>
+      </Router>
+    ), node, function () {
+      expect(this.state.location.pathname).toEqual('/nested/1/route2')
+      done()
+    })
+  })
+
+})
